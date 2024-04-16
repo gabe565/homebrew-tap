d16a100660c3a8ff1bd65edd527ed884e97ff84f
Gabe Cook <gabe565@gmail.com>
2024-04-16 04:24:54 -0500

    Brew formula update for gones version v0.0.1

---
+++ Casks/gones.rb
@@ -0,0 +1,10 @@
+cask "gones" do
+  desc "An NES emulator written in Go."
+  homepage "https://github.com/gabe565/gones"
+  version "0.0.1"
+
+  url "https://github.com/gabe565/gones/releases/download/v0.0.1/gones_darwin.tar.gz"
+  sha256 "cc2efd7fd9c2d3fe1b59da7f85ed60c75e7d91ae1d4e424d53d4c83820340f3f"
+
+  app "GoNES.app"
+end
