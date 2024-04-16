be80c6163c448e17a655664da8b0662af671a7ec
Gabe Cook <gabe565@gmail.com>
2024-04-16 03:17:52 -0500

    Brew formula update for nightscout-menu-bar version v0.0.1

---
+++ Casks/nightscout-menu-bar.rb
@@ -0,0 +1,10 @@
+cask "nightscout-menu-bar" do
+  desc "Display live blood sugar data from Nightscout in your system menu bar"
+  homepage "https://github.com/gabe565/nightscout-menu-bar"
+  version "0.0.1"
+
+  url "https://github.com/gabe565/nightscout-menu-bar/releases/download/v0.0.1/nightscout-menu-bar_darwin.tar"
+  sha256 "1713a5056da96667b4527a551616298e218d05dd67bc142faa010de33640f348"
+
+  app "Nightscout Menu Bar.app"
+end
